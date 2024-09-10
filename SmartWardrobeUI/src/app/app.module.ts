dae03ec84b4e58e90395d17310c1e6a8687db008
Gopika <51948096+gopika16@users.noreply.github.com>
2024-09-10 22:33:16 +0530

    Upgraded project to angular 17

---
+++ SmartWardrobeUI/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserImageUploadComponent } from './user-image-upload/user-image-upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserImageUploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ SmartWardrobeUI/src/app/user-image-upload/user-image-upload.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserImageUploadComponent } from './user-image-upload.component';
+
+describe('UserImageUploadComponent', () => {
+  let component: UserImageUploadComponent;
+  let fixture: ComponentFixture<UserImageUploadComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserImageUploadComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserImageUploadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

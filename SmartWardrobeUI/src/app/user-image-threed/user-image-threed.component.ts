a854fcfc2d8dd0955d345fbee1fa30049f5c07fb
Gopika <gopika16aug@gmail.com>
2024-09-14 15:38:48 +0530

    commented three js code due to errors

--- SmartWardrobeUI/src/app/user-image-threed/user-image-threed.component.ts
+++ SmartWardrobeUI/src/app/user-image-threed/user-image-threed.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import * as THREE from 'three';
+// import * as THREE from 'three';
 
 @Component({
   selector: 'app-user-image-threed',
@@ -10,10 +10,10 @@ export class UserImageThreedComponent implements AfterViewInit{
   @ViewChild('rendererCanvas', { static: false }) 
   rendererCanvas!: ElementRef<HTMLCanvasElement>;
 
-  private scene!: THREE.Scene;
-  private camera!: THREE.PerspectiveCamera;
-  private renderer!: THREE.WebGLRenderer;
-  private cube!: THREE.Mesh;
+  // private scene!: THREE.Scene;
+  // private camera!: THREE.PerspectiveCamera;
+  // private renderer!: THREE.WebGLRenderer;
+  // private cube!: THREE.Mesh;
   
   ngAfterViewInit(): void {
     this.init3DScene();
@@ -36,38 +36,38 @@ export class UserImageThreedComponent implements AfterViewInit{
   }
 
   private init3DScene() {
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, 
-      this.rendererCanvas.nativeElement.width / this.rendererCanvas.nativeElement.height, 0.1, 1000);
-    // this.camera.position.set(0, 0, 5);
-    // this.camera.lookAt(this.scene.position);  
-    this.camera.position.z = 5;
+    // this.scene = new THREE.Scene();
+    // this.camera = new THREE.PerspectiveCamera(75, 
+    //   this.rendererCanvas.nativeElement.width / this.rendererCanvas.nativeElement.height, 0.1, 1000);
+    // // this.camera.position.set(0, 0, 5);
+    // // this.camera.lookAt(this.scene.position);  
+    // this.camera.position.z = 5;
     
-    this.renderer = new THREE.WebGLRenderer({
-      canvas: this.rendererCanvas.nativeElement
-    });
+    // this.renderer = new THREE.WebGLRenderer({
+    //   canvas: this.rendererCanvas.nativeElement
+    // });
 
-    this.renderer.setSize(this.rendererCanvas.nativeElement.width, this.rendererCanvas.nativeElement.height);
+    // this.renderer.setSize(this.rendererCanvas.nativeElement.width, this.rendererCanvas.nativeElement.height);
 
-    const geometry = new THREE.PlaneGeometry(3, 3);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    this.cube = new THREE.Mesh(geometry, material);
-    // this.cube.position.set(0, 0, 0);
-    // this.cube.rotation.x = -Math.PI / 2;
-    this.scene.add(this.cube);
+    // const geometry = new THREE.PlaneGeometry(3, 3);
+    // const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    // this.cube = new THREE.Mesh(geometry, material);
+    // // this.cube.position.set(0, 0, 0);
+    // // this.cube.rotation.x = -Math.PI / 2;
+    // this.scene.add(this.cube);
   }
 
   private animate() {
-    requestAnimationFrame(() => this.animate());
-    this.renderer.render(this.scene, this.camera);
+    // requestAnimationFrame(() => this.animate());
+    // this.renderer.render(this.scene, this.camera);
   }
 
   public loadTexture(imageUrl: string) {
-    const textureLoader = new THREE.TextureLoader();
-    textureLoader.load(imageUrl, (texture) => {
-      const material = this.cube.material as THREE.MeshBasicMaterial;
-      material.map = texture;
-      material.needsUpdate = true;
-    });
+    // const textureLoader = new THREE.TextureLoader();
+    // textureLoader.load(imageUrl, (texture) => {
+    //   const material = this.cube.material as THREE.MeshBasicMaterial;
+    //   material.map = texture;
+    //   material.needsUpdate = true;
+    // });
   }
 }
